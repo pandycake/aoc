ad7d8f0a7ff6ad414ebbeaa52af64ec829587e97
andreavdh <23192178+andreavdh@users.noreply.github.com>
2022-06-22 14:30:04 +0200

    Finished 3 (rewrote 3.1)

--- 3/run.py
+++ 3/run.py
@@ -1,40 +1,60 @@
 """ https://adventofcode.com/2021/day/3 """
 
 from typing import List
+from collections import Counter
 
 
 def generate_correct_answer():
     with open("/Users/andreavandenhooff/aoc/3/input.txt", "r") as f:
         records = f.readlines()
         records = [record.strip() for record in records]
+
         print(first_assignment(records))
         print(second_assignment(records))
 
 
-def first_assignment(records: List[str]) -> int:
-    bit_count = [[0, 0] for bit in records[0]]
-
-    # Create a list with tuples counting the number of zeros and ones per index of the bit.
-    for record in records:
-        for i in range(0, len(record)):
-            if record[i] == '0':
-                bit_count[i][0] += 1
-            else:
-                bit_count[i][1] += 1
-
-    # Of each tuple, the index of the highest element of the tuple represents the more occurring bit value.
+def first_assignment(records: List[int]) -> int:
     gamma = ""
-    for i in range(0, len(bit_count)):
-        gamma += str(bit_count[i].index(max(bit_count[i])))
+
+    for i in range(0, len(records[0])):
+        dominant_bit = most_frequent(get_bits_in_nth_position(records, i))
+        gamma += dominant_bit
 
     epsilon = ''.join('1' if bit == '0' else '0' for bit in gamma)
-
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def second_assignment(records: List[str]) -> int:
-    # TODO
-    return len(records)
+def second_assignment(records: List[int]) -> int:
+    oxygen = records
+    scrubber = records
+
+    i = 0
+    while (len(oxygen) > 1):
+        dominant_bit = most_frequent(get_bits_in_nth_position(oxygen, i))
+        oxygen = [bitstring for bitstring in oxygen if bitstring[i] == dominant_bit]
+        i += 1
+
+    j = 0
+    while (len(scrubber) > 1):
+        sub_bit = least_frequent(get_bits_in_nth_position(scrubber, j))
+        scrubber = [bitstring for bitstring in scrubber if bitstring[j] == sub_bit]
+        j += 1
+
+    return int(oxygen[0], 2) * int(scrubber[0], 2)
+
+
+def get_bits_in_nth_position(records: List[str], index: int) -> List:
+    return [bitstring[index] for bitstring in records]
+
+
+def most_frequent(bits: List[str]) -> str:
+    counter = Counter(bits)
+    return '0' if counter['0'] > counter['1'] else '1'
+
+
+def least_frequent(bits: List[str]) -> str:
+    counter = Counter(bits)
+    return '0' if counter['0'] <= counter['1'] else '1'
 
 
 if __name__ == "__main__":
